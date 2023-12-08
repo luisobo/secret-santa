c36d531218fe4645f00e242397aad912e0d4f7fa
Luis Solano Bonet <contact@luissolano.com>
2023-12-08 09:18:52 -1000

    add ability to avoid couples and past pairings. cleanup pending

--- secretsanta.rb
+++ secretsanta.rb
@@ -6,14 +6,51 @@ require 'json'
 
 names = ARGV
 
+couples = JSON.parse(File.read("./couples.json")).to_h
+past = JSON.parse(File.read("./past.json"))
+
+
+
+def valid?(results, couples, past)
+
+  results.each do |k, v|
+
+    if couples[k] == v || couples[v] == k
+      return false
+    end
+
+    past.each do |p|
+      if p[k] == v
+        return false
+      end
+    end
+  end
+
+  return true
+end
+
 if names.count < 2
   puts "usage: ONE_TIME_SECRET_USERNAME=Your_OTS_username ONE_TIME_SECRET_API_KEY=Your_OTS_api_key #{__FILE__} name1 name2 [name3 ...]"
   exit 1
 end
 
-names.shuffle!
+valid = false
+results = {}
+while !valid do
+  names.shuffle!
+  results = names.zip(names.rotate).to_h
 
-results = names.zip(names.rotate).to_h
+  if valid?(results, couples, past)
+    valid = true
+    puts "VALID"
+  else
+    puts "INVALID"
+  end
+
+end
+
+# puts results
+# exit 1
 
 results.each do |k, v|
 
@@ -32,6 +69,7 @@ results.each do |k, v|
 
 end
 
-
+past << results
+File.write("./past.json", JSON.generate(past))
 
 
